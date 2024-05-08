b9a3855f52d772aec066359c2ae2d1650ea2b395
Lucas Bernadino <luucasdinoo@gmail.com>
2024-05-08 16:57:54 -0300

    Primeiro commit! - Projeto criado!

---
+++ src/test/java/com/dev/dino/workshopmongodb/WorkshopSpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.dino.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
