b8de4463dbd9b2140cc7bc2e673909a4bec3c82c
Lucas Bernadino <luucasdinoo@gmail.com>
2024-05-09 15:19:47 -0300

    User DTO

--- src/main/java/com/dev/dino/workshopmongodb/Controllers/UserController.java
+++ src/main/java/com/dev/dino/workshopmongodb/Controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.dev.dino.workshopmongodb.Controllers;
 
+import com.dev.dino.workshopmongodb.dto.UserDTO;
 import com.dev.dino.workshopmongodb.entities.User;
 import com.dev.dino.workshopmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -18,8 +20,9 @@ public class UserController {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
