8e9a769ad734d9d4c52ceba5c755009e9cbbeedd
Lucas Bernadino <luucasdinoo@gmail.com>
2024-05-10 13:19:39 -0300

    Add comentarios nos posts

--- src/main/java/com/dev/dino/workshopmongodb/entities/Post.java
+++ src/main/java/com/dev/dino/workshopmongodb/entities/Post.java
@@ -1,11 +1,14 @@
 package com.dev.dino.workshopmongodb.entities;
 
 import com.dev.dino.workshopmongodb.dto.AuthorDTO;
+import com.dev.dino.workshopmongodb.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 @Document
 public class Post implements Serializable {
@@ -16,6 +19,7 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
     public Post(){}
 
     public Post(String id, Date date, String title, String body, AuthorDTO author) {
@@ -64,6 +68,10 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
     }
 
     @Override
